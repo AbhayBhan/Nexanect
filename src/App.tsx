30b7f543c9ac16d5d38280f388f540cc24b7aa70
AbhayBhan <abhaybhan197@gmail.com>
2024-08-01 20:23:57 +0530

    Initial Setup ✨

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Coming Soon.</h1>
+      <Button>Hello</Button>
+    </>
+  )
+}
+
+export default App
